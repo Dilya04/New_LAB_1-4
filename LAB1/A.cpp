6989fcf8db29ef5b9939235d644a7e963748b11d
Dilya04 <145293268+Dilya04@users.noreply.github.com>
2023-10-02 17:36:26 +0600

    Create A.cpp

---
+++ LAB1/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a;
+
+    cin >> b;
+
+    double c = sqrt(a * a + b * b);
+
+    cout << "Гипотенуза: " << c << endl;
+
+    return 0;
+}
