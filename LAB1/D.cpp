c546ba8d99184ebe079cbbb4a5ac0d5beb6ee43e
Dilya04 <145293268+Dilya04@users.noreply.github.com>
2023-10-02 17:39:46 +0600

    Create D.cpp

---
+++ LAB1/D.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int N, K , ostatok;
+	cout << "N:";
+	cin >> N;
+	cout << "K:";
+	cin >> K;
+	ostatok = K % N;
+	cout << "apples " << ostatok ;
+
+
+}
