9053a055b9ff073d0ccbf1cc496fb7ad36908c2f
Dilya04 <145293268+Dilya04@users.noreply.github.com>
2023-10-05 23:40:16 +0600

    Create H.cpp

---
+++ LAB3/H.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int sum=0;
+	for (int i = 0; i <= n; i++) {
+		sum += pow(2, i);  
+	}
+	cout << sum;
+}
+
